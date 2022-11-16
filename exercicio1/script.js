f9e3fecbd1c673cd4ea573a6381b33732c3f251b
Renan <ren4n_oliveira@hotmail.com>
2022-11-16 20:09:10 -0300

    Exercicios atualizados

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,12 +1,13 @@
 const variavel = Number(prompt("Digite um número para descobrir se é divisivel por 2 ou 3"))
 
  if (variavel %2 === 0){
-    console.log("É divisivel por 2")}
-    else if (variavel %3 === 0){
-    console.log("É divisivel por 3")
-     }
+    console.log("É divisivel por 2")
+    if (variavel %3 === 0)
+    console.log("É divisivel por 3")}
+     
 
-else (console.log("Não é divisível por 2 ou 3"))
+else if (variavel %3 === 0){ console.log("Esse número não é divisivel por 2 mas é por 3")}
+else console.log("Não é divisivel por 2 e por 3")
   
 /* 
 if (variavel %2 ===0 || variavel %3 ===0){
